4db47b15bb710e92a0d5357bdf1b29f3bdaf6382
David Vezzani <david.vezzani@greenseedtech.com>
2018-02-19 14:42:58 -0700

    introduces environment scopes

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,83 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const DashboardPlugin = require('webpack-dashboard/plugin');
 require('dotenv').config()
 
-const extractSass = new ExtractTextPlugin({
-		filename: "styles.css",
-		allChunks: true,
-});
+module.exports = (env) => {
+	const ENV = (typeof env !== 'undefined' && env.APP_ENV) || process.env.APP_ENV || 'dev';
+	const isTest = ENV === 'test'
+	const isProd = ENV === 'prod';
+	
+	const extractSass = new ExtractTextPlugin({
+			filename: "styles.css",
+			allChunks: true,
+		  disable: ENV === "dev"
+	});
+	
+	console.log(['ENV', ENV]);
+		
+	function setDevTool() {  // function to set dev-tool depending on environment
+			if (isTest) {
+				return 'inline-source-map';
+			} else if (isProd) {
+				return 'source-map';
+			} else {
+				return 'eval-source-map';
+			}
+	}
 
-module.exports = {
-  entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
-  output: {
-    path: __dirname + '/dist', // Folder to store generated bundle
-    filename: 'bundle.js',  // Name of generated bundle after build
-    publicPath: '/' // public URL of the output directory when referenced in a browser
-  },
-  module: {  // where we defined file patterns and their loaders
-      rules: [ 
-          {
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: [
-              /node_modules/
-            ]
-          }, 
-          {
-              test: /\.html/,
-              loader: 'raw-loader'
-          },
-          {
-            test: /\.(sass|scss)$/,
-            use: [{
-                loader: "style-loader" // creates style nodes from JS strings
-            }, {
-                loader: "css-loader" // translates CSS into CommonJS
-            }, {
-                loader: "sass-loader" // compiles Sass to CSS
-            }]
-          }, 
-					{
-						test: /\.s?css$/,
-						use: extractSass.extract({
-								use: [{
-										loader: "css-loader"
-								}, {
-										loader: "sass-loader"
-								}],
-								// use style-loader in development 
-								fallback: "style-loader"
-						})
-					},
-      ]
-  },
-  plugins: [  // Array of plugins to apply to build chunk
-      new HtmlWebpackPlugin({
-          template: __dirname + "/src/public/index.html",
-          inject: 'body'
-      }), 
-			extractSass, // extract css to a separate file called styles.css
-			new webpack.DefinePlugin({  // plugin to define global constants
-				API_KEY: JSON.stringify(process.env.API_KEY)
-			}), 
-			new DashboardPlugin(), 
-  ],
-  devServer: {  // configuration for webpack-dev-server
-      contentBase: './src/public',  //source of static assets
-      port: 7700, // port to run dev-server
-  } 
+	const config = {
+		devtool: setDevTool(),  //Set the devtool
+
+		entry: __dirname + "/src/app/index.js", // webpack entry point. Module to start building dependency graph
+		output: {
+			path: __dirname + '/dist', // Folder to store generated bundle
+			filename: 'bundle.js',  // Name of generated bundle after build
+			publicPath: '/' // public URL of the output directory when referenced in a browser
+		},
+		module: {  // where we defined file patterns and their loaders
+				rules: [ 
+						{
+							test: /\.js$/,
+							use: 'babel-loader',
+							exclude: [
+								/node_modules/
+							]
+						}, 
+						{
+								test: /\.html/,
+								loader: 'raw-loader'
+						},
+						{
+							test: /\.s?[ac]ss$/,
+							use: extractSass.extract({
+									use: [{
+											loader: "css-loader"
+									}, {
+											loader: "sass-loader"
+									}],
+									// use style-loader in development 
+									fallback: "style-loader"
+							})
+						},
+				]
+		},
+		plugins: [  // Array of plugins to apply to build chunk
+				new HtmlWebpackPlugin({
+						template: __dirname + "/src/public/index.html",
+						inject: 'body'
+				}), 
+				extractSass, // extract css to a separate file called styles.css
+				new webpack.DefinePlugin({  // plugin to define global constants
+					API_KEY: JSON.stringify(process.env.API_KEY)
+				}), 
+		],
+		devServer: {  // configuration for webpack-dev-server
+				contentBase: './src/public',  //source of static assets
+				port: 7700, // port to run dev-server
+		} 
+	};
+
+	return config;
 };
 
